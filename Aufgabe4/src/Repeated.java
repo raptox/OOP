d18351000c6b2bd73218a7c91abbdd4e1f52533f
raptox <raptox91@gmail.com>
2012-11-13 23:34:54 +0100

    repeated, scaled

--- Aufgabe4/src/Repeated.java
+++ Aufgabe4/src/Repeated.java
@@ -1,11 +1,90 @@
-public class Repeated<P> extends AbstractToString {
-	private P[][] elements;
-	
+public class Repeated<P> implements Pict {
+	protected P[][] elements;
+	protected int width, height;
+	protected double swidth, sheight;
+
+
 	public Repeated( P[][] elements ) {
 		this.elements = elements;
+		width = elements.length;
+		height = elements[0].length;
+		swidth = width;
+		sheight = height;
 	}
-	
+
 	public void scale( double factor ) {
+		swidth = swidth*factor;
+		sheight = sheight*factor;
+	}
+
+	public String toString() {
+		int iswidth = (int)Math.ceil(swidth);
+		int isheight = (int)Math.ceil(sheight);
 		
+		
+		Zeilen[][] helper = new Zeilen[(int)iswidth][(int)isheight];
+		int maxWidth = 0;
+		int maxHeight = 0;
+		String returnString = "";
+		
+		P[][] newElements = (P[][]) new Object[iswidth][isheight];
+		
+		for (int i=0; i<iswidth; i++) {
+			for (int y=0; y<isheight; y++) {
+				newElements[i][y] = elements[i%width][y%height]; 	
+			}
+		}
+		
+		// helper mit den toStrings fuellen, berchnet auch die Zeilenlaenge
+		// und laengste Zeile suchen, damit wir wissen wie viel wir auffuellen
+		for (int i=0; i<iswidth; i++) {
+			for (int y=0; y<isheight; y++) {
+				helper[i][y] = new Zeilen(newElements[i][y].toString());
+
+				// laengste Zeile ausfindig machen
+				if (helper[i][y].getZeilenLaenge() > maxWidth)
+					maxWidth = helper[i][y].getZeilenLaenge();
+
+				// maxHeight bestimmen
+				if (helper[i][y].getAnzZeilen() > maxHeight)
+					maxHeight = helper[i][y].getAnzZeilen();
+			}
+		}
+		
+		// Objekte ausgeben und auffuellen
+		for (int i=0; i<iswidth; i++) {
+			for (int h=0; h<maxHeight; h++) {
+				for (int y=0; y<isheight; y++) {
+					String nextLine = helper[i][y].getNextLine();
+					returnString = returnString.concat(nextLine);
+					//System.out.println("i: "+i+" y: "+y+" nextLine: "+nextLine);
+
+					int lineLength = helper[i][y].getZeilenLaenge();
+					int stringHeight = helper[i][y].getAnzZeilen();
+					
+					if (lineLength < maxWidth) {
+						if (nextLine.equals("")) {
+							for (int z=0; z<maxWidth; z++) {
+								returnString=returnString.concat(" ");
+							}
+						} else {
+							for (int z=0; z<maxWidth-lineLength; z++) {
+								returnString=returnString.concat(" ");
+							}							
+						}
+					} else if (nextLine.equals("") && stringHeight < maxHeight) {
+						for (int z=0; z<maxWidth; z++)
+							returnString=returnString.concat(" ");
+					}
+				}
+				//System.out.println("i: "+i+" line: "+returnString);
+				returnString=returnString.concat("\n");
+			}
+			//returnString.concat("\n");
+		}
+
+
+
+		return returnString;
 	}
 }
--- Aufgabe4/src/Scaled.java
+++ Aufgabe4/src/Scaled.java
@@ -1,12 +1,16 @@
-public class Scaled<P extends Pict> extends AbstractToString {
-	private P[][] elements;
+public class Scaled<P extends Pict> extends Repeated implements Pict {
 	
 	public Scaled( P[][] elements ) {
-		this.elements = elements;
+		super(elements);
 	}
 	
 	@Override
 	public void scale(double factor) {
-		
+		for (int i=0; i<elements.length; i++) {
+			for (int y=0; y<elements[0].length; y++) {
+				((P)elements[i][y]).scale(factor);
+			}
+		}
 	}
+	
 }
