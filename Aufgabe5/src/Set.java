316087e5434c8e18bd1a19d48c387a9b4c817dce
Bernd Artmüller <berndartmueller@gmail.com>
2012-11-20 19:30:14 +0100

    SetIterator wieder als private innere klasse

--- Aufgabe5/src/Set.java
+++ Aufgabe5/src/Set.java
@@ -1,4 +1,5 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Set<P> implements java.lang.Iterable<P> {
 	protected LinkedList<P> list;
@@ -25,6 +26,62 @@ public class Set<P> implements java.lang.Iterable<P> {
 	
 	@Override
 	public Iterator<P> iterator() {
-		return new SetIterator<P>( this.list );
+		return new SetIterator( this.list );
+	}
+	
+	private class SetIterator implements Iterator<P> {
+		private Item<P> current;
+		private LinkedList<P> list;
+		
+		public SetIterator( LinkedList<P> list ) {
+			this.list    = list;
+			this.current = this.list.getRoot();
+		}
+		
+		@Override
+		public boolean hasNext() {
+			if ( this.current != null ) {
+				return true;
+			}
+			
+			return false;
+		}
+
+		@Override
+		public P next() {
+			P value = this.current.getValue();
+			
+			if ( this.current == null ) { 
+				throw new NoSuchElementException();
+			}
+			this.current = this.current.getNext();
+			
+			return value;
+		}
+
+		@Override
+		public void remove() {
+			Item<P> hCurrent = this.list.getRoot();
+			Item<P> itemBefore = hCurrent;
+			
+			while( hCurrent != null ) {
+				// aktuelles Element gefunden
+				if ( hCurrent == this.current ) {
+					if ( hCurrent == this.list.getRoot() ) {
+						this.list.setRoot( null );
+						this.current = null;
+					}
+					// akutelles Element wird aus Liste rausgehängt
+					itemBefore.setNext( hCurrent.getNext() );
+				}
+				
+				// voriges Element zwischenspeichern
+				itemBefore = hCurrent;
+				// Zeiger auf nächstes Element verschieben
+				hCurrent = hCurrent.getNext();
+				
+				this.current = itemBefore.getNext();
+			}
+		}
 	}
 }
