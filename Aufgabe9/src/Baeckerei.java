206c20732de0d5e5ac68793e69082c5d29d76181
raptox <raptox91@gmail.com>
2012-12-18 15:24:23 +0100

    keksdose, baeckerei

--- Aufgabe9/src/Baeckerei.java
+++ Aufgabe9/src/Baeckerei.java
@@ -1,3 +1,44 @@
-public class Baeckerei {
+import java.util.ArrayList;
 
+/*
+ * diese Klasse nimmt Bestellungen auf und erzeugt Kekse wenn die Bestellungen geliefert werden
+ */
+public class Baeckerei {
+	private String name;
+	private ArrayList bestellungen;
+	
+	/*
+	 * Konstruktor
+	 * VB: name != null
+	 */
+	public Baeckerei(String name) {
+		this.name = name;
+		bestellungen = new ArrayList();
+	}
+	
+	/*
+	 * Fuegt neue Bestellung hinzu
+	 * VB: a != null
+	 */
+	public void neueBestellung(Bestellung a) {
+		bestellungen.add(a);
+	}
+	
+	/*
+	 * Zeigt alle Bestellungen an
+	 * NB: liefert String mit allen Bestellungen
+	 */
+	public String zeigeBestellungen() {
+		String best = "";
+		
+		for (int i=0; i<bestellungen.size(); i++) {
+			best = best + "Bestellung "+ i + ":\n" + bestellungen.get(i) + "\n";
+		}
+		
+		return best;
+	}
+	
+	public Kecksdose liefereBestellungen() {
+		
+	}
 }
